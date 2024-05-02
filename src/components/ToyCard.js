b0945f9de2630dd25f8a5a02164814213703cd41
PeterMbugua <pmbugua276@gmail.com>
2024-05-02 12:39:52 +0530

    done

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-function ToyCard() {
+function ToyCard({ toy, onDelete }) {
+
+  const [likes, setLikes] = useState(toy.likes);
+
+  const handleLike = () => {
+    const updatedLikes = likes + 1;
+
+    axios.patch(`http://localhost:3001/toys/${toy.id}`, { likes: updatedLikes })
+      .then(() => {
+        setLikes(updatedLikes);
+        console.log("Likes updated successfully!");
+      })
+      .catch(error => {
+        console.error('Error updating likes:', error);
+      });
+  };
+
+
+  const handleDelete = () => {
+    axios.delete(`http://localhost:3001/toys/${toy.id}`)
+      .then(() => {
+        onDelete(toy.id);
+        console.log("Toy deleted successfully!");
+        window.location.reload();
+      })
+      .catch(error => {
+        console.error('Error deleting toy:', error);
+      });
+  };
+
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{toy.name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={toy.image}
+        alt={toy.name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{likes} Likes </p>
+      <button className="like-btn" onClick={handleLike}>Like {"❤️"}</button>
+      <button className="del-btn" onClick={handleDelete}>Donate to GoodWill</button>
     </div>
   );
 }
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
-function ToyForm() {
+function ToyForm(addaToy) {
+
+  const [formData, setFormData] = useState({
+    name:"",
+    image:"",
+  });
+  function handleClick (event) {
+    setFormData({
+      ...formData,
+      [event.target.name]:event.target.value
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const newToy = {
+      ...formData,
+      Likes: ""
+    }
+
+    axios.post('http://localhost:3001/toys',newToy)
+      .then ((response) => addaToy(response.data) )
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
+          onChange={handleClick}
+          value={formData.name}
           placeholder="Enter a toy's name..."
           className="input-text"
         />
@@ -15,6 +41,8 @@ function ToyForm() {
         <input
           type="text"
           name="image"
+          onChange={handleClick}
+          value={formData.image}
           placeholder="Enter a toy's image URL..."
           className="input-text"
         />
